2fddc66aaee2f54172f5c5a70c0fabb62494deb3
Bohdana K <bohdana.dev@gmail.com>
2024-10-08 23:15:10 +0300

    Create navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,31 @@
 import './navbar.scss';
 
 const Navbar = () => {
-  return <div className='navbar'>Navbar</div>;
+  return (
+    <div className='navbar'>
+      <div className='logo'>
+        <img src='logo.svg' alt='logo' />
+        <span>ADMIN</span>
+      </div>
+      <div className='icons'>
+        <img src='/search.svg' alt='' className='icon' />
+        <img src='/app.svg' alt='' className='icon' />
+        <img src='/expand.svg' alt='' className='icon' />
+        <div className='notification'>
+          <img src='/notifications.svg' alt='notifications' />
+          <span>1</span>
+        </div>
+        <div className='user'>
+          <img
+            src='https://images.unsplash.com/photo-1584513707958-07bf650b9026?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            alt='user'
+          />
+          <span>User</span>
+        </div>
+        <img src='/settings.svg' alt='' className='icon' />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
